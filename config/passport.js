c5e35e478183bb2e1ab40f31540eabef35ce7e11
Seth Bergman <seth.atxwebs@gmail.com>
2016-06-21 21:21:06 -0500

    Added passport strategies

--- config/passport.js
+++ config/passport.js
@@ -4,11 +4,9 @@ var TwitterStrategy  = require('passport-twitter').Strategy;
 var GoogleStrategy   = require('passport-google-oauth').OAuth2Strategy;
 var GitHubStrategy   = require('passport-github2').Strategy;
 
+var User       = require('../models/user');
 var configAuth = require('./auth');
 
-//===============PASSPORT=================
-
-// Passport session setup.
 module.exports = function(passport) {
 
 passport.serializeUser(function(user, done) {
@@ -74,10 +72,11 @@ function ensureAuthenticated(req, res, next) {
   res.redirect('/signin');
 }
 
+/*
 passport.use(new GitHubStrategy({
-    clientID: 'd511dcdcb8e6f14946e1',
-    clientSecret: '253fcb69955ceb42ad50a47ecc120e3a9f08941a',
-    callbackURL: "http://dashboard-ejs.stackriot.com/auth/github/callback"
+    clientID: '4e723bed0302f6ebc195',
+    clientSecret: 'bfe3ca4fbd099b1b854bd34e39bbe0aa5f9bdc69',
+    callbackURL: "http://dashboard-hbs.stackriot.com/auth/github/callback"
   },
   function(req, accessToken, refreshToken, profile, done) {
     User.findOne({ 'github.id' : profile.id }, function (err, user) {
@@ -85,6 +84,7 @@ passport.use(new GitHubStrategy({
     });
   }
 ));
+*/
 
 // =========================================================================
 // GITHUB ================================================================
@@ -153,6 +153,86 @@ function(req, token, refreshToken, profile, done) {
             user.github.token = token;
             user.github.name  = profile.name;
             user.github.email = (profile.emails[0].value || '').toLowerCase();
+
+            user.save(function(err) {
+                if (err)
+                    return done(err);
+
+                return done(null, user);
+            });
+
+        }
+    });
+
+}));
+
+// =========================================================================
+// FACEBOOK ================================================================
+// =========================================================================
+passport.use(new FacebookStrategy({
+
+    clientID        : configAuth.facebookAuth.clientID,
+    clientSecret    : configAuth.facebookAuth.clientSecret,
+    callbackURL     : configAuth.facebookAuth.callbackURL,
+    profileFields   : ['id', 'name', 'email'],
+    passReqToCallback : true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
+
+},
+function(req, token, refreshToken, profile, done) {
+
+    // asynchronous
+    process.nextTick(function() {
+
+        // check if the user is already logged in
+        if (!req.user) {
+
+            User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+                if (err)
+                    return done(err);
+
+                if (user) {
+
+                    // if there is a user id already but no token (user was linked at one point and then removed)
+                    if (!user.facebook.token) {
+                        user.facebook.token = token;
+                        user.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+                        user.facebook.email = (profile.emails[0].value || '').toLowerCase();
+
+                        user.save(function(err) {
+                            if (err)
+                                return done(err);
+
+                            return done(null, user);
+                        });
+                    }
+
+                    return done(null, user); // user found, return that user
+                } else {
+                    // if there is no user, create them
+                    var newUser            = new User();
+
+                    newUser.facebook.id    = profile.id;
+                    newUser.facebook.token = token;
+                    newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+                    newUser.facebook.email = (profile.emails[0].value || '').toLowerCase();
+
+                    newUser.save(function(err) {
+                        if (err)
+                            return done(err);
+
+                        return done(null, newUser);
+                    });
+                }
+            });
+
+        } else {
+            // user already exists and is logged in, we have to link accounts
+            var user            = req.user; // pull the user out of the session
+
+            user.facebook.id    = profile.id;
+            user.facebook.token = token;
+            user.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+            user.facebook.email = (profile.emails[0].value || '').toLowerCase();
 
             user.save(function(err) {
                 if (err)
